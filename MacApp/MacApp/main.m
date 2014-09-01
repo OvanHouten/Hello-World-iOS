795f7503aaf72bbb8774ec52a27299a5e4df6bc9
OvanHouten <olaf.ajax@hotmail.com>
2014-09-01 13:06:36 +0200

    First Commit
    First Commit bitches

---
+++ MacApp/MacApp/main.m
@@ -0,0 +1,24 @@
+//  main.m
+//  MacApp
+//
+//  Olaf van Houten
+//  10424598
+//
+//  Created by Olaf Van Houten on 01/09/14.
+//  Copyright (c) 2014 Olaf van Houten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, FirstApp!");
+        
+    }
+    return 0;
+}
+
